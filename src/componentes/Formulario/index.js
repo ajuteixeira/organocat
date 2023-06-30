5d7c6dadd76e2030bdb415875c4a828189ef41ca
Juliana Teixeira <teixeirajuuh@gmail.com>
2023-06-30 16:27:12 -0300

    dropdown list

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,32 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+
+  const times = [
+    'Selecionar...',
+    'Gordinhos',
+    'Danados',
+    'Preguiçosos'
+  ]
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault()
+  }
+
   return (
     <section className='formulario'>
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do gatinho :)</h2>
-        <CampoTexto label="Nome" placeholder="Digite o nome do gatinho" />
-        <CampoTexto label="Raça" placeholder="Digite a raça do gatinho" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o nome do gatinho" />
+        <CampoTexto obrigatorio={true} label="Raça" placeholder="Digite a raça do gatinho" />
         <CampoTexto label="Imagem" placeholder="Digite o link da imagem do gatinho" />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <Botao>
+          Criar card
+        </Botao>
       </form>
     </section>
   )
